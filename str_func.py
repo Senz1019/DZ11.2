644e886a686a34fbf4855c32b6affd12d4ed1cdb
Senz1019 <Kaius10@yandex.ru>
2023-07-16 12:59:00 +0300

    add docstring for 1 func, add 2 func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,4 @@
+"""Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами"""
 def convert_to_uppercase(string):
     return string.upper()
 
@@ -5,4 +6,17 @@ def convert_to_uppercase(string):
 input_string = input()
 output_string = convert_to_uppercase(input_string)
 print(output_string)
+
+"""Новая функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+
+def capitalize_words(input_string):
+    words = input_string.split()  # Разделение строки на список слов
+    capitalized_words = [word.capitalize() for word in words]  # Преобразование каждого слова в список с заглавной буквы
+    output_string = ' '.join(capitalized_words)  # Объединение слов обратно в строку с пробелами
+    return output_string
+
+# Пример использования функции
+input_str = "hello world! how are you?"
+capitalized_str = capitalize_words(input_str)
+print(capitalized_str)
 
