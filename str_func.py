1020ba76e71aaf5abd7c8f339601eb1bc2c2f7e2
Senz1019 <Kaius10@yandex.ru>
2023-07-16 12:15:03 +0300

    create str_func.py and add func convert_to_upper

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def convert_to_uppercase(string):
+    return string.upper()
+
+# Пример использования функции
+input_string = input()
+output_string = convert_to_uppercase(input_string)
+print(output_string)
+
